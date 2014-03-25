7004a8ade3c024c6ac143841e0646ce1e1c4338a
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-03-25 17:50:55 -0600

    JS works reasonably well

--- static/js/email_sort.js
+++ static/js/email_sort.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   $('#id_email_order').hide();
-  $('#id_email_order').before('<div id="id_sortable_email"></div>');
+  $('#id_email_order').before('<div id="id_sortable_email"><h1>Please save before sorting.</h1></div>');
+  $('#id_emails').change(function() {
+    $('#id_sortable_email').html('<h1>Please save before sorting.</h1>');
+  });
   render_list();
 });
 
@@ -9,20 +12,22 @@ function render_list() {
   $('#id_emails option:selected').each(function(index, email) {
     view.email.push({'id': $(email).val(), 'name':  $(email).text()});
   });
-  $.post('/static/templates/email_list.html', function(mustache_template) {
-    $('#id_sortable_email').html(Mustache.render(mustache_template, view));
-    setup_sortable();
-  });
+  if(view.email.length) {
+    $.post('/static/templates/email_list.html', function(mustache_template) {
+      $('#id_sortable_email').html(Mustache.render(mustache_template, view));
+      setup_sortable();
+    });
+  }
 }
 
 function setup_sortable() {
-    $('#sortable').sortable();
-    $('#sortable').disableSelection();
-    $('#id_email_order').text($('#sortable').sortable('serialize'));
+  $('#sortable').sortable();
+  $('#sortable').disableSelection();
+  $('#id_email_order').text($('#sortable').sortable('serialize'));
 
-    $('#sortable').sortable(
-      { change: function() {
-        $('#id_email_order').text($('#sortable').sortable('serialize'));
-      }
-    });
+  $('#sortable').sortable(
+    { update: function(event, ui) {
+      $('#id_email_order').text($('#sortable').sortable('serialize'));
+    }
+  });
 }
