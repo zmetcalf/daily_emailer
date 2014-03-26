fcc866174e8c940c768d1e6e4b6d59f176fa4f77
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-03-26 16:07:01 -0600

    Added security on ajax request - wrote tests

--- daily_emailer/tests.py
+++ daily_emailer/tests.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import Client
 
@@ -6,6 +9,9 @@ from daily_emailer.models import Email, EmailGroup
 class AjaxAssociatedEmailTests(TestCase):
 
     def setUp(self):
+        user = User.objects.create_user('Admin', 'admin@sample.com', 'password')
+        user.is_staff = True
+        user.save()
         eq = EmailGroup(group_name='NewRep')
         eq.save()
         eq.email_set.create(subject='Subject1', message='Message1')
@@ -15,5 +21,20 @@ class AjaxAssociatedEmailTests(TestCase):
         self.client = Client()
 
     def test_ajax_associated_emails(self):
+        self.client.login(username='Admin', password='password')
+        response = self.client.post('/associated_emails/1/')
+        data = json.loads(response.content)
+        self.assertEqual(data[0]['pk'], 1)
+        self.assertEqual(data[0]['fields']['message'], 'Message1')
+        self.assertEqual(data[1]['fields']['message'], 'Message2')
+        self.assertEqual(data[2]['fields']['message'], 'Message3')
+
+    def test_ajax_associated_emails_authenticated(self):
+        self.client.login(username='Admin', password='password')
         response = self.client.post('/associated_emails/1/')
         self.assertEqual(response.status_code, 200)
+
+    def test_ajax_associated_emails_unauthenticated(self):
+        response = self.client.post('/associated_emails/1/')
+        self.assertEqual(response.status_code, 404)
+
--- daily_emailer/views.py
+++ daily_emailer/views.py
@@ -1,10 +1,12 @@
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_list_or_404
 
 from daily_emailer.models import Email
 
 def ajax_associated_emails(request, group_id):
+    if not request.user.is_staff:
+        raise Http404
     emails = get_list_or_404(Email, email_group_id=group_id)
     data = serializers.serialize('json', emails)
     return HttpResponse(data, content_type='application/json')
