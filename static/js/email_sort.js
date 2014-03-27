61e0f6e8104c7420a7bebf99f6e846a2e96d4b05
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-03-27 17:46:46 -0600

    Doing some serious work with backbone to sort emails

--- static/js/email_sort.js
+++ static/js/email_sort.js
@@ -1,13 +1,95 @@
+var OrderedList = Backbone.Model.extend({
+
+  initialize: function() {
+    Backbone.Model.prototype.initialize.apply(this, arguments);
+
+    _.bindAll(this, 'merge_order');
+
+    this.get_order();
+    this.email_list = $.ajax(('/associated_emails/' +
+      $('.field-id > div > p').text() + '/'), {
+        type: 'POST',
+        success: this.merge_order
+      });
+  },
+
+  get_order: function() {
+    this.set('order_list', _.map($('#id_email_order').text().split(",") || [],
+             function(value) {
+                return parseInt(value);
+             })
+    );
+  },
+
+  set_order: function() {
+    $('#id_email_order').text(this.get('order_list'));
+  },
+
+  merge_order: function(json_list) {
+    var order_list = this.get('order_list');
+    var additions = [];
+    var subtractions = [];
+    var list = [];
+
+    _.each(json_list, function(item) {
+      list.push(item.pk);
+    });
+
+    _.each(order_list, function(list_item) {
+      if(!_.contains(list, list_item)) {
+        order_list.splice(order_list.indexOf(list_item), 1);
+      }
+    });
+    _.each(list, function(list_item) {
+      if(!_.contains(order_list, list_item)) {
+        order_list.push(list_item);
+      }
+    });
+
+    this.set_order();
+    this.sort_and_set_emails();
+    //this.render_list();
+  },
+
+  sort_and_set_emails: function() {
+    console.log(this.email_list);
+    var order_list = this.get('order_list');
+    this.email_list.sort(function(email) {
+      return _.indexOf(order_list, email.pk)
+    });
+  },
+
+  render_list: function() {
+    var view = { 'email': [] };
+    _.each(this.email_list, function(index, email) {
+      view.email.push({'id': $(email).val(), 'name':  $(email).text()});
+    });
+    if(view.email.length) {
+      $.post('/static/templates/email_list.html', function(mustache_template) {
+        $('#id_sortable_email').html(Mustache.render(mustache_template, view));
+        setup_sortable();
+      });
+    }
+  }
+});
+
 $(document).ready(function() {
+  $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+      xhr.setRequestHeader('X-CSRFToken', $('input[name="csrfmiddlewaretoken"]').val());
+    }
+  });
   $('#id_email_order').hide();
   $('.field-id').hide();
-  if($('.field-id').children().children().text() === 'ID:(None)') {
+  if($('.field-id > div > p').text() === '(None)') {
     $('.field-email_order').hide();
   } else {
     $('#id_email_order').before('<div id="id_sortable_email"></div>');
-    render_list();
+    order_list = new OrderedList;
   }
 });
+
+
 
 function render_list() {
   var view = { 'email': [] };
--- static/js/js_tests.js
+++ static/js/js_tests.js
@@ -1,3 +1,37 @@
-test("render_list", function() {
+var simulated_ajax_response = [{"pk": 1, "model": "daily_emailer.email", "fields": {"message": "Message1", "email_group": 1, "subject": "Subject1"}}, {"pk": 3, "model": "daily_emailer.email", "fields": {"message": "Message3", "email_group": 1, "subject": "Subject3"}}];
+var myAPI = function() { return $.Deferred().promise(simulated_ajax_response); };
+var ajax_stub = sinon.stub($,'ajax', myAPI);
 
+test("merge_list_no_update", function() {
+  var ordered_list = new OrderedList;
+  ordered_list.merge_order(simulated_ajax_response);
+  deepEqual(ordered_list.get('order_list'), [3,1]);
+  deepEqual($('#id_email_order').text(), '3,1');
+});
+
+test("merge_list_addition", function() {
+  $('#id_email_order').text('3');
+  var ordered_list = new OrderedList;
+  deepEqual(ordered_list.get('order_list'), [3]);
+  ordered_list.merge_order(simulated_ajax_response);
+  deepEqual(ordered_list.get('order_list'), [3,1]);
+  deepEqual($('#id_email_order').text(), '3,1');
+});
+
+test("merge_list_subtraction", function() {
+  $('#id_email_order').text('3,2,1');
+  var ordered_list = new OrderedList;
+  deepEqual(ordered_list.get('order_list'), [3,2,1]);
+  ordered_list.merge_order(simulated_ajax_response);
+  deepEqual(ordered_list.get('order_list'), [3,1]);
+  deepEqual($('#id_email_order').text(), '3,1');
+});
+
+test("sort_and_set_emails", function() {
+  $('#id_email_order').text('3,1');
+  var ordered_list = new OrderedList;
+  ordered_list.merge_order(simulated_ajax_response);
+  ordered_list.sort_and_set_emails();
+  deepEqual(ordered_list.email_list[0].pk, 3);
+  deepEqual(ordered_list.email_list[1].pk, 1);
 });
