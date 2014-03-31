3c5f136d1736fa424325d91ccc2f07083c5102e2
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-03-31 17:17:22 -0600

    Fixed errors in send_daily_email and started writing tests

--- daily_emailer/management/commands/send_daily_email.py
+++ daily_emailer/management/commands/send_daily_email.py
@@ -1,31 +1,57 @@
+import datetime
+
 from django.core.management.base import BaseCommand, CommandError
 
 from daily_emailer import models
 
 class Command(BaseCommand):
-    args = '<campaign_id campaign_id ...?'
-    help = 'Handels sending out daily email'
+    help = 'Handles sending out daily email'
 
-    def handle(self, *args, **options):
-        for campaign_id in args:
-            try:
-                campaign = models.Campaign.objects.get(pk=int(campaign_id))
-            except models.Campaign.DeosNotExist:
-                raise CommandError('Campaign "%s" does not exist' % campaign_id)
+    def handle_noargs(self):
+        campaign_list = models.Campaign.objects.all()
+        if not campaign_list:
+            return
 
+        for campaign in campaign_list:
             if campaign.completed_date:
-                return
+                continue
 
             try:
-                emails = Email.objects.all().filter(
+                emails = models.Email.objects.all().filter(
                     email_group_id=campaign.email_group.pk)
-            except Email.DoesNotExist:
-                return # TODO Create test for this case
+            except models.Email.DoesNotExist:
+                continue
 
             email_order = list(map(int, campaign.email_group.email_order.strip('[]').split(',')))
 
-            next_email = get_next_email(emails, email_order, campaign.status)
+            if len(email_order) != len(email_order):
+                email_order = self.reconcile_emails(emails,
+                                campaign.email_group.email_order)
+                campaign.email_group.email_order = email_order
+                campaign.email_group.save()
 
+            next_email = self.get_next_email(emails, email_order, campaign.status)
+
+            if next_email:
+                self.send_email(next_email, campaign.recipient)
+                campaign.status[next_email.pk] = str(datetime.date.today())
+            else:
+                campaign.completed_date = datetime.date.today()
+            campaign.save()
+
+    # If user adds or removes an email before sorting, it needs fixed
+    def reconcile_emails(self, emails, email_order):
+        email_pk_list = []
+        for email in emails:
+            email_pk_list.append(email.pk)
+            if not email.pk in email_order:
+                email_order.append(email.pk)
+        for list_item in email_order:
+           if not list_item in email_pk_list:
+              email_order.remove(list_item)
+        return email_order
+
+    # Algorithm to deterine next email to be sent
     def get_next_email(self, emails, email_order, sent_dict):
         for ordered_email in email_order:
             if not ordered_email in sent_dict.keys():
@@ -33,3 +59,6 @@ class Command(BaseCommand):
                     if email.pk == ordered_email:
                         return email
         return False
+
+    def send_email(self, email, recipient):
+        pass
--- daily_emailer/tests.py
+++ daily_emailer/tests.py
@@ -1,6 +1,9 @@
+import datetime
 import json
+import unittest
 
 from django.contrib.auth.models import User
+from django.core.management.base import  CommandError
 from django.test import TestCase
 from django.test.client import Client
 from django.utils import timezone
@@ -68,16 +71,77 @@ class StatusFieldTests(TestCase):
 class SendDailyEmailTests(TestCase):
 
     def setUp(self):
+        recipient = models.Recipient(first_name='John', last_name='Smith',
+                                     email='sample@email.com')
+        recipient.save()
         eq = models.EmailGroup(group_name='NewRep', email_order='3,1,2')
         eq.save()
         eq.email_set.create(subject='Subject1', message='Message1')
         eq.email_set.create(subject='Subject2', message='Message2')
         eq.email_set.create(subject='Subject3', message='Message3')
         eq.save()
+        campaign = models.Campaign(reference_name='RefName',
+                                   status='{3: "2014-4-1"}',
+                                   start_date=timezone.now(), email_group = eq,
+                                   recipient = recipient)
+        campaign.save()
         self.cmd = send_daily_email.Command()
 
-    def test_get_next_email(self):
+    def test_get_next_email_success(self):
         email_order = models.EmailGroup.objects.get(group_name='NewRep')
         emails = models.Email.objects.all().filter(email_group=email_order)
         self.assertEqual(self.cmd.get_next_email(emails, [3,1,2],
                          {3: '2014-4-1'}).pk, 1)
+
+    def test_get_next_email_fail(self):
+        email_order = models.EmailGroup.objects.get(group_name='NewRep')
+        emails = models.Email.objects.all().filter(email_group=email_order)
+        self.assertEqual(self.cmd.get_next_email(emails, [3,1,2],
+                         {3: '2014-4-1', 2: '2014-4-2', 1: '2014-4-3'}), False)
+
+    def test_reconcile_emails_addition(self):
+        email_order = models.EmailGroup.objects.get(group_name='NewRep')
+        emails = models.Email.objects.all().filter(email_group=email_order)
+        self.assertEqual(self.cmd.reconcile_emails(emails, [3,1]), [3,1,2])
+
+    def test_reconcile_emails_subtraction(self):
+        email_order = models.EmailGroup.objects.get(group_name='NewRep')
+        emails = models.Email.objects.all().filter(email_group=email_order)
+        self.assertEqual(self.cmd.reconcile_emails(emails, [5,3,1,4,2]), [3,1,2])
+
+    def test_reconcile_emails_no_change(self):
+        email_order = models.EmailGroup.objects.get(group_name='NewRep')
+        emails = models.Email.objects.all().filter(email_group=email_order)
+        self.assertEqual(self.cmd.reconcile_emails(emails, [3,1,2]), [3,1,2])
+
+    def test_reconcile_emails_add_and_sub(self):
+        email_order = models.EmailGroup.objects.get(group_name='NewRep')
+        emails = models.Email.objects.all().filter(email_group=email_order)
+        self.assertEqual(self.cmd.reconcile_emails(emails, [3,4,2]), [3,2,1])
+
+    @unittest.skip('working on others')
+    def test_handle_noargs_addition(self):
+        self.cmd.handle_noargs()
+        campaign_list = models.Campaign.objects.all()
+        self.assertEqual(campaign_list[0].status,
+                         {3: '2014-4-1', 1: str(datetime.date.today())})
+
+    @unittest.skip('working on others')
+    def test_handle_noargs_subtraction(self):
+        self.cmd.handle_noargs()
+        campaign_list = models.Campaign.objects.all()
+        self.assertEqual(campaign_list[0].status,
+                         {3: '2014-4-1', 1: str(datetime.date.today())})
+
+    def test_handle_noargs_no_change(self):
+        self.cmd.handle_noargs()
+        campaign_list = models.Campaign.objects.all()
+        self.assertEqual(campaign_list[0].status,
+                         {3: '2014-4-1', 1: str(datetime.date.today())})
+
+    @unittest.skip('working on others')
+    def test_handle_noargs_add_and_sub(self):
+        self.cmd.handle_noargs()
+        campaign_list = models.Campaign.objects.all()
+        self.assertEqual(campaign_list[0].status,
+                         {3: '2014-4-1', 1: str(datetime.date.today())})
