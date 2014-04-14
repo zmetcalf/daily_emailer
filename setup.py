1098ffaa2d3555ba1bb263d35d3f0f28fe2e3e2b
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-04-14 16:31:07 -0600

    Attempt at making it installable with PIP

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(name='daily_emailer',
       version='alpha',
@@ -6,5 +6,8 @@ setup(name='daily_emailer',
       author='Zach Metcalf',
       author_email='zachery.metcalf@gmail.com',
       url='http://github.com/zmetcalf/daily_emailer',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
       install_requires=['django>=1.6.2', 'sendgrid>=0.3.5'],
      )
