465a23399e7d1ac9467e809c9b858b02990dbaa2
Zachery Metcalf <zachery.metcalf@gmail.com>
2014-05-02 08:10:56 -0600

    Starting to fix campaign view

--- daily_emailer/urls.py
+++ daily_emailer/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('daily_emailer.views',
     url(r'^associated_emails/(?P<group_id>\d+)/$',
         'ajax_associated_emails'),
-    url(r'^campaign_emails/(?P<campaign>\d+)/$',
-        'ajax_campaign_emails'),
+    url(r'^sent_emails/(?P<campaign>\d+)/$',
+        'ajax_sent_emails'),
     url(r'^js_tests/', 'js_tests'),
 )
--- daily_emailer/views.py
+++ daily_emailer/views.py
@@ -1,8 +1,8 @@
 from django.core import serializers
 from django.http import Http404, HttpResponse
-from django.shortcuts import  render
+from django.shortcuts import get_object_or_404, get_list_or_404, render
 
-from daily_emailer.models import Campaign, Email
+from daily_emailer.models import Campaign, Email, SentEmail
 
 def ajax_associated_emails(request, group_id):
     if not request.user.is_staff:
@@ -14,17 +14,27 @@ def ajax_associated_emails(request, group_id):
     data = serializers.serialize('json', emails)
     return HttpResponse(data, content_type='application/json')
 
-def ajax_campaign_emails(request, campaign):
+def ajax_sent_emails(request, campaign):
     if not request.user.is_staff:
         raise Http404
-    try:
-        campaign = Campaign.objects.get(pk=campaign)
-        emails = Email.objects.all().filter(
-            email_group_id=campaign.email_group.pk)
-    except (Email.DoesNotExist, Campaign.DoesNotExist):
-        return HttpResponse('[]', content_type='application/json')
-    data = serializers.serialize('json', emails)
-    return HttpResponse(data, content_type='application/json')
+    _campaign = get_object_or_404(Campaign, pk=campaign)
+    emails = get_list_or_404(Email,
+        email_group_id=_campaign.email_group.pk)
+
+    sent_emails = get_list_or_404(SentEmail, campaign=_campaign)
+
+    email_list = {}
+
+    for email in emails:
+        for se in sent_emails:
+            if se.email == email:
+                email_list[email] = { email.subject: se.sent_date }
+                continue
+        email_list[email] = { email.subject: '' }
+
+    print email_list
+
+    return render(request, 'daily_emailer/status.html', email_list)
 
 def js_tests(request):
     return render(request, 'daily_emailer/js_tests.html')
